40c85fe167e1e8f69b424e7ab0dd6b9ed452d369
Dominik Völkel <voelkel.dominik@gmail.com>
2024-12-04 14:09:46 +0100

    Add gravity, proper jump, and relative cam controls

---
+++ gameObjects/scripts/CameraMovement.gd
@@ -0,0 +1,37 @@
+extends Node3D
+
+var mouse_cam_sensitivity = 0.005
+var joystick_cam_sensitivity = 0.1
+var twist_input = 0.0
+var pitch_input = 0.0
+var input_dir
+
+@onready var twist_pivot = $h
+@onready var pitch_pivot = $h/v
+
+func _physics_process(delta):
+	# joystick compatability
+	input_dir = Input.get_vector("Look left", "Look right", "Look up", "Look down")
+	twist_pivot.rotate_y(-input_dir.x * joystick_cam_sensitivity)
+	pitch_pivot.rotate_x(-input_dir.y * joystick_cam_sensitivity)
+	
+	# mouse compatability
+	# cam pivot rotations are gotten from the _unhandled_input func below
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	
+	# make sure player can't rotate the camera too far up or down
+	pitch_pivot.rotation.x = clamp(
+		pitch_pivot.rotation.x,
+		deg_to_rad(-90),
+		deg_to_rad(45)
+	)
+	twist_input = 0.0
+	pitch_input = 0.0
+
+func _unhandled_input(event):
+	# inputs for mouse
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = -event.relative.x * mouse_cam_sensitivity
+			pitch_input = -event.relative.y * mouse_cam_sensitivity
---
+++ gameObjects/scripts/PlayerMovement.gd
@@ -0,0 +1,70 @@
+# This script just deals with player movement, camera movement is in the CamOrigin script.
+
+extends CharacterBody3D
+
+var speed = 5.0
+var jump_velocity = 10.0
+var turn_speed = 30
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var parent
+
+@onready var cam_h = $"../CamOrigin/h"
+@onready var cam_v = $"../CamOrigin/h/v"
+
+func _ready():
+	# set mouse mode captured so it locks in middle of screen
+	capture_mouse()
+	
+	# set parent
+	parent = get_parent()
+
+func _unhandled_input(event):
+	# if "exit" (esc) is pressed, close game
+	if Input.is_action_just_pressed("Exit"):
+		get_tree().quit()
+
+func _process(delta):
+	# set parent position to equal CharacterBody position every frame
+	parent.position = position
+
+func _physics_process(delta):
+	# turn player inputs into a vector
+	var input_dir = Input.get_vector("Player left", "Player right", "Player Forward", "Player Back")
+	
+	# use input_dir movement vector to know which direction the player is facing, depending on the camera rotation 
+	# put .normalized() at the end of this line if you don't want the player to be able to slow walk using joystick
+	var direction = (cam_h.transform.basis * Vector3(input_dir.x, 0, input_dir.y))
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("Player jump") and is_on_floor():
+		velocity.y = jump_velocity      
+	
+	# if playerBody isn't on the floor, activate gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	
+	
+	if direction:
+		# player movement
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		
+		# change player model rotation
+		rotation.y = lerp_angle(rotation.y, atan2(-direction.x, -direction.z), turn_speed * delta)
+	
+	else:
+		# if no direction input, make sure player is still
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	
+	move_and_slide()
+
+func capture_mouse():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func release_mouse():
+	# not used anywhere in this demo, but you probably will want to run this func for menus and stuff
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)

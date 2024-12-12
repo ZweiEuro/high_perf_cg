7d96e3fc52a6d6ef3e72a40b8a979c4258320595
Dominik Völkel <voelkel.dominik@gmail.com>
2024-12-12 19:55:12 +0100

    quad sub branching

--- alt_lightning/multi_mesh_instance_3d.gd
+++ alt_lightning/multi_mesh_instance_3d.gd
@@ -9,7 +9,7 @@ extends MeshInstance3D
 
 @export var thickness: float = 0.1;
 
-@onready var direction = (end_point_offset - start_point_offset).normalized()
+@onready var main_bolt_direction = (end_point_offset - start_point_offset).normalized()
 @onready var segment_length = (end_point_offset - start_point_offset).length() / n_segments
 
 @onready var rel_start_point : Vector3 = start_point_offset + self.position;
@@ -17,10 +17,12 @@ extends MeshInstance3D
 
 @onready var self_remove_timer: Timer = Timer.new();
 
+@onready var main_bolt = Array()
+
 func offsetVector(vec: Vector3) -> Vector3:
 	return vec + Vector3(1.0, 0, 0) * thickness
 
-func append_quad(arr: Array, p1: Vector3) -> void:
+func append_quad(arr: Array, p1: Vector3, direction: Vector3) -> void:
 	
 	var tl: Vector3;
 	var tr: Vector3;
@@ -33,7 +35,7 @@ func append_quad(arr: Array, p1: Vector3) -> void:
 		tr = arr[-2];
 	
 	
-	var bl = p1 + self.direction * self.segment_length;
+	var bl = p1 + direction * self.segment_length;
 	var br = offsetVector(bl);
 	
 	arr.append(tl);
@@ -44,76 +46,63 @@ func append_quad(arr: Array, p1: Vector3) -> void:
 	arr.append(br);
 	arr.append(tr);
 	
-	
 
-func _generate_lightning() -> Array:
-	var main_bolt = Array()
+func jitter_point(point: Vector3) ->Vector3:
+	return point + Vector3((randf()*2-1) * jaggedness, 0,(randf()*2-1) * jaggedness)
+
+
+func generate_main_bolt() -> void:
 	var branches = []
-
-	append_quad(main_bolt, start_point_offset);
-	
- 
+	append_quad(main_bolt, start_point_offset, main_bolt_direction);
 	for i in range(n_segments + 1):				
-		var point = start_point_offset + direction * segment_length * i
+		var point = start_point_offset + main_bolt_direction * segment_length * i
 					
 		# random displacement 
 		point += Vector3((randf()*2-1) * jaggedness, 0,(randf()*2-1) * jaggedness)
 		
-		append_quad(main_bolt, point);
+		append_quad(main_bolt, point, main_bolt_direction);
+	
 
-		# branch check
-		if i > 1 and randf() < branch_chance:
-			var branch_count = randi() % 2 + 1  # 1-2 branches per point
-			for j in range(branch_count):
-				# Limit branch angles to stay near vertical -> don't know how, too tired to question
-				var branch_dir = direction.rotated(Vector3.UP, randf() * 0.5 - 0.25).rotated(Vector3.RIGHT, randf() * 0.2 - 0.1)
-				var branch_length = segment_length * randf() * 1.5
-				# !!! v comment back in
-				# branches.append(_generate_branch(point, branch_dir, branch_length, jaggedness * 0.5, 2, max_branch_depth - 1))
 
-	return [main_bolt, branches]
+func generate_branch(branch_point: Vector3, depth = self.max_branch_depth) -> void:
+	if depth <= 0:
+		return;
+	
+	var branch: Array = [];
+	var branch_dir = main_bolt_direction.rotated(Vector3.UP, randf() * 0.5 - 0.25).rotated(Vector3.RIGHT, randf() * 0.2 - 0.1)
+	
+	append_quad(branch, branch_point, branch_dir);
+	
+	for i in range(4):
+		var next_point = branch_point + branch_dir * segment_length;
+		next_point = jitter_point(next_point);
+	
+		append_quad(branch, next_point, branch_dir);
 
-func _generate_branch(base_point: Vector3, direction: Vector3, length: float, jaggedness: float, segments: int, depth: int) -> Array:
-	var branch_points = Array()
-	for i in range(segments + 1):
-		var point = base_point + direction * length * (i / segments)
+	self.main_bolt += branch
+
+func generate_branches() -> void:
+	for i in range(0, main_bolt.size(), 6):
+		if randf() < branch_chance:
+			# actually branch
+			generate_branch(main_bolt[i]);
 		
-		point += Vector3((randf()*2-1) * jaggedness, 0,(randf()*2-1) * jaggedness)
-		branch_points.append(point)
 
-		# recursive branches 
-		if depth > 0 and i > 1 and randf() < branch_chance:
-			var sub_branch_dir = direction.rotated(Vector3.UP, randf() * 0.4 - 0.2).rotated(Vector3.RIGHT, randf() * 0.2 - 0.1)
-			var sub_branch_length = length * randf() * 0.5
-			branch_points.append_array(_generate_branch(point, sub_branch_dir, sub_branch_length, jaggedness * 0.5, segments, depth - 1))
 
-	return branch_points
+
 
 func create_mesh():
-	var lightning_data = _generate_lightning() 
-	var main_bolt = lightning_data[0]
-	var branches = lightning_data[1]
-
 	var mesh = ImmediateMesh.new()
-
-	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLES)
 	
+	generate_main_bolt() 
+	generate_branches();
+	
+	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLES)
+
 	for point in main_bolt:
 		mesh.surface_add_vertex(point)
 	mesh.surface_end()
 	
-	print(main_bolt)
-	
-	self.mesh = mesh
-	return;
-	
-	# Draw branches
-	for branch in branches:
-		mesh.surface_begin(Mesh.PRIMITIVE_LINE_STRIP)
-		for point in branch:
-			mesh.surface_add_vertex(point)
-		mesh.surface_end()
-
 	self.mesh = mesh
 
 func _ready():
